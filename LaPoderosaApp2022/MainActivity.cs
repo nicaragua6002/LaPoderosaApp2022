210d3a4af25b4f6c125577f903bc5dbf1997ded7
Roberto <nicaragua_6002@yahoo.es>
2022-05-04 14:03:03 -0600

    Con Historia, Mision e Internacionalización - Funcionales.

--- LaPoderosaApp2022/MainActivity.cs
+++ LaPoderosaApp2022/MainActivity.cs
@@ -10,15 +10,34 @@ namespace LaPoderosaApp2022
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        Button btn1;
+        Button btn1, btn2,btn3;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            //Inicio
             btn1 = FindViewById<Button>(Resource.Id.Button1);
             btn1.Click += Btn1_Click;
+            //Historia
+            btn2 = FindViewById<Button>(Resource.Id.Button2);
+            btn2.Click += Btn2_Click;
+            //Mision
+            btn3 = FindViewById<Button>(Resource.Id.Button3);
+            btn3.Click += Btn3_Click;
+        }
+
+        private void Btn3_Click(object sender, System.EventArgs e)
+        {
+            Intent i = new Intent(this, typeof(ActivityMision));
+            StartActivity(i);
+        }
+
+        private void Btn2_Click(object sender, System.EventArgs e)
+        {
+            Intent i = new Intent(this, typeof(ActivityHistoria));
+            StartActivity(i);
         }
 
         private void Btn1_Click(object sender, System.EventArgs e)
